e407b3d1fd4be4b8b5d65a0b54821e9c22d4f569
shabanovacpp <67010004+shabanovacpp@users.noreply.github.com>
2022-09-04 17:42:25 +0300

    Add files via upload

---
+++ portfolio/js/script.js
@@ -0,0 +1,5 @@
+
+document.getElementsByClassName('icon_mail')[0].onclick = function () {
+  document.getElementsByClassName('icon_mail')[0].classList.toggle('active');
+  document.getElementsByClassName('text_mail')[0].classList.toggle('active');
+}
